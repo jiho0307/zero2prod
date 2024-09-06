2037c7bea3595a858e7a6ed5a48bdf2f699e736f
jiho0307 <jiho0307@gmail.com>
2024-09-06 22:29:03 +0900

    end of chapter7-7-5
    Signed-off-by: jiho0307 <jiho0307@gmail.com>

---
+++ migrations/20240906120305_create_subscriptions_tokens_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE subscriptions_tokens(
+    subscriptions_token TEXT NOT NULL,
+    subscriptions_id uuid NOT NULL
+        REFERENCES subscriptions (id),
+    PRIMARY KEY (subscriptions_token)
+)
